e8e1e466a85a6eb8299e22603dd6417980d9a765
りぃる <100053953+reel-nico@users.noreply.github.com>
2022-04-29 06:53:25 +0900

    Update create.js

--- create/create.js
+++ create/create.js
@@ -121,7 +121,7 @@ async function onEncrypt() {
 
   const encrypted = await generateFragment(url, password, hint, useRandomSalt,
       useRandomIv);
-  const output = `https://jstrieb.github.io/link-lock/#${encrypted}`;
+  const output = `https://link-lock.reel-official.ml/#${encrypted}`;
 
   document.querySelector("#output").value = output;
   highlight("output");
